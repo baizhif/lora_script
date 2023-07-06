a8d17a3f592f88f0ff832103be1ad72d60fd729f
baizhif <135608974+baizhif@users.noreply.github.com>
2023-07-06 11:34:38 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, Response, make_response, send_from_directory
+from flask import render_template
+from flask import request
+from werkzeug.utils import secure_filename
+
+from someMethods import parseTrain, Tasks
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('mainui.html')
+
+@app.route('/favicon.ico',methods=['GET'])
+def getIco():
+    return make_response(send_from_directory("templates","sharefolder_desktop_link.ico",as_attachment=True))
+
+@app.route('/test/upload',methods=['POST'])
+def processUpload():
+    save_dir = request.headers.get("Path")
+    if os.path.exists(save_dir) is False:
+        os.makedirs(save_dir)
+    uploaded_files  = request.files.getlist("file")
+    [file.save(os.path.join(save_dir,secure_filename(file.filename))) for file in uploaded_files]
+    return 'ok'
+
+
+@app.route('/newTask',methods=['POST'])
+def addNewTask():
+    cmd = parseTrain(request.form.to_dict())
+    return t.addTask(cmd)
+
+if __name__ == '__main__':
+    t = Tasks()
+    app.run(port=7860)
